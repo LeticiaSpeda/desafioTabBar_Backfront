793880ce8b26251a17e4451c161f42de5249800f
Leticia Speda <leticiaspeda@gmail.com>
2023-09-29 17:04:19 -0300

    feat: adicionado componentes de contato

--- DesafioTabBar/DesafioTabBar/Views/PersonViewController.swift
+++ DesafioTabBar/DesafioTabBar/Views/PersonViewController.swift
@@ -13,19 +13,59 @@ final class PersonViewController: UIViewController {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 10
-//        stack.backgroundColor = .black
         stack.translate()
         return stack
     }()
     
+    private lazy var photoView: UIView = {
+        let view = UIView()
+        view.translate()
+        return view
+    }()
+    
     private lazy var userPhotoImage: UIImageView = {
-        let image = UIImage.user
-        let imgView = UIImageView(image: image)
-        imgView.layer.masksToBounds = true
-//        imgView.layer.cornerRadius = 60 / 2
+        let imgView = UIImageView(image: .teste)
         imgView.contentMode = .scaleAspectFit
+        
+        imgView.backgroundColor = .red
+        imgView.layer.masksToBounds = true
+        imgView.layer.cornerRadius = 150 / 2
         imgView.translate()
         return imgView
+    }()
+    
+    private lazy var selectImageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Editar Foto", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        return button
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Nome"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private lazy var nameTextFiel: UITextField = {
+        let label = UITextField()
+        label.placeholder = "Digite seu nome"
+        label.backgroundColor = .white
+        label.layer.cornerRadius = 6
+        label.translate()
+        return label
+    }()
+    
+    private lazy var addImageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Adicionar", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -44,8 +84,13 @@ final class PersonViewController: UIViewController {
         
         informationsView.addSubview(mainVStackView)
         
-//        mainVStackView.addArrangedSubview(UIView())
-        mainVStackView.addArrangedSubview(userPhotoImage)
+        mainVStackView.addArrangedSubview(photoView)
+        photoView.addSubview(userPhotoImage)
+        mainVStackView.addArrangedSubview(selectImageButton)
+        mainVStackView.addArrangedSubview(nameLabel)
+        mainVStackView.addArrangedSubview(nameTextFiel)
+        mainVStackView.addArrangedSubview(UIView())
+        mainVStackView.addArrangedSubview(addImageButton)
         mainVStackView.addArrangedSubview(UIView())
     }
     
@@ -55,15 +100,19 @@ final class PersonViewController: UIViewController {
             informationsView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             informationsView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             informationsView.heightAnchor.constraint(equalToConstant: 400),
-//            informationsView.bottomAnchor.constraint(equalTo: mainVStackView.bottomAnchor),
+            informationsView.bottomAnchor.constraint(equalTo: mainVStackView.bottomAnchor),
             
             mainVStackView.topAnchor.constraint(equalTo: informationsView.topAnchor, constant: 10),
-            mainVStackView.leadingAnchor.constraint(equalTo: informationsView.leadingAnchor),
-            mainVStackView.trailingAnchor.constraint(equalTo: informationsView.trailingAnchor),
-            mainVStackView.bottomAnchor.constraint(equalTo: informationsView.bottomAnchor),
+            mainVStackView.leadingAnchor.constraint(equalTo: informationsView.leadingAnchor, constant: 10),
+            mainVStackView.trailingAnchor.constraint(equalTo: informationsView.trailingAnchor, constant: -10),
             
-            userPhotoImage.heightAnchor.constraint(equalToConstant: 100),
-            userPhotoImage.widthAnchor.constraint(equalToConstant: 100),
+            photoView.heightAnchor.constraint(equalToConstant: 150),
+            
+            userPhotoImage.centerXAnchor.constraint(equalTo: photoView.centerXAnchor),
+            userPhotoImage.heightAnchor.constraint(equalToConstant: 150),
+            userPhotoImage.widthAnchor.constraint(equalToConstant: 150),
+            
+            nameTextFiel.heightAnchor.constraint(equalToConstant: 28),
             
         ])
     }
